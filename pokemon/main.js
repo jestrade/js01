4509535e185b683bf0c66f66f0ec8154d5982b2c
Jesus Estrada <jesuestrada@gmail.com>
2025-04-06 21:42:10 -0500

    organize

--- pokemon/main.js
+++ pokemon/main.js
@@ -1,51 +1,168 @@
-const users = [
-    { email: 'admin', password: "admin" },
-    { email: 'admin2', password: "admin" },
-    { email: 'admin3', password: "admin" }
-];
+const apiURL = "";
+const pokemonAPIURL = "https://pokeapi.co/api/v2/pokemon?limit=10";
 
 // initial load
 const onload = () => {
-    const user = localStorage.getItem("user");
-    if (user) {
+    const token = localStorage.getItem("token");
+    if (token) {
         loadData();
     } else {
-        const form = document.getElementById("form");
+        const form = document.getElementById("loginForm");
         form.style.display = "block";
 
-        const results = document.getElementById("results");
-        results.style.display = "none";
+        const list = document.getElementById("list");
+        list.style.display = "none";
     }
 }
 
-const logout = () => {
-    localStorage.removeItem("user");
+
+const showRegister = () => {
+    const message = document.getElementById("message");
+    message.innerHTML = "";
+
+    const form = document.getElementById("registerForm");
+    form.style.display = "block";
+
+    const list = document.getElementById("loginForm");
+    list.style.display = "none";
+}
+
+const showLogin = () => {
+    const message = document.getElementById("message");
+    message.innerHTML = "";
+
+    const form = document.getElementById("loginForm");
+    form.style.display = "block";
+
+    const list = document.getElementById("registerForm");
+    list.style.display = "none";
+}
+
+const showList = () => {
+    const message = document.getElementById("message");
+    message.innerHTML = "";
+
+    const form = document.getElementById("loginForm");
+    form.style.display = "none";
+
+    const list = document.getElementById("list");
+    list.style.display = "block";
+}
+
+const logoutUser = () => {
+    localStorage.removeItem("token");
     location.reload();
 }
 
-const login = () => {
+const handleLogin = async () => {
+    const message = document.getElementById("message");
+    message.innerHTML = "";
+
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+
+    if (!username || !password) {
+        message.innerHTML = "Please enter username and password";
+        message.style.color = "red";
+
+        return;
+    }
+
+    const url = `${apiURL}/users/login`;
+    const data = {
+        username: username,
+        password: password
+    };
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    const responseData = await response.json();
+
+    if (response.ok) {
+        const token = responseData?.data?.token;
+        if (!token) {
+            message.innerHTML = "Token not found";
+            message.style.color = "red";
+            return;
+        }
+        localStorage.setItem("token", token);
+        showList();
+        loadData();
+    } else {
+        message.innerHTML = responseData.message;
+        message.style.color = "red";
+    }
+
+    document.getElementById("loginPassword").value = "";
+}
+
+const handleRegister = async () => {
     const message = document.getElementById("message");
     message.innerHTML = "";
 
     const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+    const username = document.getElementById('username').value;
+    const passwordConfirmation = document.getElementById('passwordConfirmation').value;
     const password = document.getElementById('password').value;
-    const user = users.find(user => user.email === email && user.password === password);
 
-    if (user) {
-        localStorage.setItem("user", JSON.stringify(user));
-        
-        loadData();
+    if (!email || !name || !username || !password || !passwordConfirmation) {
+        message.innerHTML = "Please enter email, name, username, password and password confirmation";
+        message.style.color = "red";
+        return;
+    }
+    if (password !== passwordConfirmation) {
+        message.innerHTML = "Password and password confirmation do not match";
+        message.style.color = "red";
+        return;
+    }
+
+    const url = `${apiURL}/users/`;
+
+    const data = {
+        name,
+        email,
+        username,
+        password,
+        passwordConfirmation
+        }
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (response.ok) {
+        document.getElementById("email").value = "";
+        document.getElementById("name").value = "";
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
+        document.getElementById("passwordConfirmation").value = "";
+
+        message.innerHTML = "User created successfully";
+        message.style.color = "green";
     } else {
-        message.innerHTML = "not found";
+        const responseData = await response.json();
+        message.innerHTML = responseData.message;
+        message.style.color = "red";
     }
 }
 
 const loadData = () => {
-    const form = document.getElementById("form");
+    const form = document.getElementById("loginForm");
         form.style.display = "none";
 
         const table = document.getElementById("table");
-        const url = "https://pokeapi.co/api/v2/pokemon/";
+        const url = pokemonAPIURL;
 
         fetch(url)
         .then(response => response.json())
@@ -59,6 +176,6 @@ const loadData = () => {
                 cell2.innerHTML = pokemon.url;
             });
         });
-        const results = document.getElementById("results");
-        results.style.display = "block";
+        const list = document.getElementById("list");
+        list.style.display = "block";
 }
